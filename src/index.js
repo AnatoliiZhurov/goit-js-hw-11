42b35d4e682f84d1015f2f512088300b282323ae
AnatoliiZhurov <129226759+AnatoliiZhurov@users.noreply.github.com>
2023-09-26 02:43:24 +0300

    ticket to pass

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(SimpleLightbox);
 
+import { fetchPix } from './partials/pix-api';
+
 let requestWord;
 let pageNum = 1;
 const LOCAL_KEY = 'search-word';
@@ -84,54 +86,6 @@ async function loadMore() {
   }
 }
 
-async function fetchPix(requestWord) {
-  const BASE_URL = `https://pixabay.com/api/`;
-  const API_KEY = `39642997-acb236756d532ae28c959f640`;
-  const params = {
-    key: API_KEY,
-    q: requestWord,
-    image_type: `photo`,
-    orientation: `horizontal`,
-    safesearch: true,
-    page: pageNum,
-    per_page: 40,
-  };
-
-  const resp = await axios.get(BASE_URL, { params });
-
-  const totalHits = resp.data.totalHits;
-
-  const data = resp.data.hits;
-
-  if (data.length === 0) {
-    throw new Error(error);
-  }
-
-  const result = data.map(
-    ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      return {
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      };
-    }
-  );
-
-  return { result, totalHits }; // Повертаємо об'єкт з результатами і загальною кількістю
-}
-
 function createMarkup({ result }) {
   return result
     .map(
@@ -163,3 +117,5 @@ function createMarkup({ result }) {
     )
     .join(``);
 }
+
+export { pageNum };
