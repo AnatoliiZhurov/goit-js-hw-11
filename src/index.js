333bae0e4ac3fd33b5a86859129543d3a45d0ee4
AnatoliiZhurov <129226759+AnatoliiZhurov@users.noreply.github.com>
2023-09-26 23:16:50 +0300

    dell log

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(SimpleLightbox);
 
 import { fetchPix } from './partials/pix-api';
 
